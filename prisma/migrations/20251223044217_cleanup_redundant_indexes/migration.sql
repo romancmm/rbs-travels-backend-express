9c8966536cba3eebd4b6b4d0ec869b0637b2cde3
romancmm <romancmm@gmail.com>
2025-12-23 10:48:15 +0600

    Update schema

---
+++ prisma/migrations/20251223044217_cleanup_redundant_indexes/migration.sql
@@ -0,0 +1,38 @@
+-- DropIndex
+DROP INDEX "public"."Category_name_idx";
+
+-- DropIndex
+DROP INDEX "public"."Category_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."Menu_cacheKey_idx";
+
+-- DropIndex
+DROP INDEX "public"."Menu_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."MenuItem_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."Page_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."PageBuilder_cacheKey_idx";
+
+-- DropIndex
+DROP INDEX "public"."PageBuilder_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."Post_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."Project_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."Service_slug_idx";
+
+-- DropIndex
+DROP INDEX "public"."Setting_key_idx";
+
+-- DropIndex
+DROP INDEX "public"."User_email_idx";
